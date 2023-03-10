01f49d02c873011aba3d59302310f79b7180311a
Nisha Goel <nishagoel2001@gmail.com>
2023-03-11 02:25:51 +0530

    Profile page event cards updated

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -131,6 +131,68 @@ export default function Profile() {
           </form>
         </div>
       </div>
+      <h1 className="h1 text-center mt-5 mb-4">EVENTS</h1>
+      <div className="row ">
+        <div className="col-lg-4 col-md-6">
+          <div
+            className={`card ${extend.eventCard} mt-4 mb-5`}
+            onClick={() => {
+              window.location.href = '/';
+            }}
+          >
+            <img
+              className="card-img-top"
+              src="https://ai.iti.gov.eg/wp-content/uploads/2020/05/event.jpg"
+              alt="CardImage"
+            />
+            <div className="card-body">
+              <div className="card-text CardText text-center">
+                <h1 className="h2 text-center mt-3">EventName</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-lg-4 col-md-6">
+          <div
+            className={`card ${extend.eventCard} mt-4 mb-5`}
+            onClick={() => {
+              window.location.href = '/';
+            }}
+          >
+            <img
+              className="card-img-top"
+              src="https://ai.iti.gov.eg/wp-content/uploads/2020/05/event.jpg"
+              alt="CardImage"
+            />
+            <div className="card-body">
+              <div className="card-text CardText text-center">
+                <h1 className="h2 text-center mt-3">EventName</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-lg-4 col-md-6">
+          <div
+            className={`card ${extend.eventCard} mt-4 mb-5`}
+            onClick={() => {
+              window.location.href = '/';
+            }}
+          >
+            <img
+              className="card-img-top"
+              src="https://ai.iti.gov.eg/wp-content/uploads/2020/05/event.jpg"
+              alt="CardImage"
+            />
+            <div className="card-body">
+              <div className="card-text CardText text-center">
+                <h1 className="h2 text-center mt-3">EventName</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
