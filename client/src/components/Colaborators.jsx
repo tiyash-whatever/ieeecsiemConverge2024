3af4204526aece0e07f5735bb4e351c0d6204f84
Anirban Bhattacharjee <71592780+AnirbanB02@users.noreply.github.com>
2023-03-15 03:02:15 +0530

    collaborators added

--- client/src/components/Colaborators.jsx
+++ client/src/components/Colaborators.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+// importing logos
+import cslogo from "../assets/img/collaborators/ieeeiemCSLogo.jpg"
+import ieeeLogo from "../assets/img/collaborators/IEEELogo.jpg"
+import iemLogo from "../assets/img/collaborators/iemLogo.jfif"
+import iiciem from "../assets/img/collaborators/iiciem.png"
+import talyeah from "../assets/img/collaborators/TALYEAH.png"
 
 const Colaborators = () => {
   return (
@@ -6,10 +12,26 @@ const Colaborators = () => {
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <div className="skill-bx wow zoomIn">
+            <div className="skill-bx wow zoomIn ">
               <h2>Collaborators</h2>
               {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p> */}
-              <h3 style={{color:"#00ffcc"}} className="display-4">Coming Soon</h3>
+              <div className="collab row flex align-items-center justify-content-center mt-3 ">
+                <div className="col-md-2">
+                  <img src={cslogo} alt="" />
+                </div>
+                <div className="col-md-2">
+                  <img src={ieeeLogo} alt="" />
+                </div>
+                <div className="col-md-2">
+                  <img src={iemLogo} alt="" />
+                </div>
+                <div className="col-md-2">
+                  <img src={iiciem} alt="" />
+                </div>
+                <div className="col-md-2">
+                  <img src={talyeah} alt="" />
+                </div>
+              </div>
             </div>
           </div>
         </div>
